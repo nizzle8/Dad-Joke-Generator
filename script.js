df1209e826f8531e96789e4453fa5f180895ca00
Mathew Edwards <mathew.edwards@hotmail.co.uk>
2022-10-03 19:06:47 +0200

    updated with async function

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ const jokeBtn = document.getElementById("jokeBtn");
 generateJoke();
 jokeBtn.addEventListener("click", generateJoke);
 
-function generateJoke() {
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             "Accept": "application/json"
+//         }   
+//     }
+
+//     fetch("https://icanhazdadjoke.com", config)
+//     .then(res => res.json())
+//     .then(data => {
+//         jokeEl.innerHTML = data.joke
+//     });
+// };
+
+async function generateJoke() {
     const config = {
         headers: {
             "Accept": "application/json"
         }   
     }
+    const res = await fetch("https://icanhazdadjoke.com", config);
+    const data = await res.json();
 
-    fetch("https://icanhazdadjoke.com", config)
-    .then(res => res.json())
-    .then(data => {
-        jokeEl.innerHTML = data.joke
-    });
+    jokeEl.innerHTML = data.joke
 };
